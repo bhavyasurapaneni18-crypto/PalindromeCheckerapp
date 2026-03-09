11a2a575d5c5cd1d8c5649641fb6f53c34be6035
bhavya <bhavyasurapaneni18@gmail.com>
2026-03-09 12:10:44 +0530

    Base application setup

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 class main {
     public static void main(String[]args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Welcome to Paplindrome checker App");
+        System.out.println("Welcome to Palindrome checker App");
 
     }
 }
